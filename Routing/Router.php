e30f6aa1d454ac1c932dd7540cddfd4220e7950e
SickMyDucks <me@neilrichter.com>
2018-04-11 21:38:19 +0200

    Rewrite rule fixed

--- Routing/Router.php
+++ Routing/Router.php
@@ -8,12 +8,11 @@ class Router
     public function execute($request)
     {
         global $config;
-
         if (preg_match($config['profile'], $request))
         {
             $data = explode(':', $config['routes']['profile']);
-            $controller_name = "Controller\\".$data[0].'Controller';
-            $method_name = $data[1].'Action';
+            $controller_name = "Controller\\".'UserController';
+            $method_name = 'profileAction';
             
             $controller = new $controller_name;
             $response = call_user_func([$controller, $method_name]);
@@ -28,7 +27,6 @@ class Router
             
             $controller = new $controller_name;
             $response = call_user_func([$controller, $method_name]);
-            
             return $response;
         }
         else
